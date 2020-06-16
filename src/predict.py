e5e22b2485e5974389537eb05a66a6603a619133
Junzheng <alchemistWu0521@gmail.com>
2020-06-16 17:05:06 -0400

    add width and height control

--- src/predict.py
+++ src/predict.py
@@ -150,11 +150,18 @@ if __name__ == '__main__':
     args.add_argument('-pie', '--pie', type=int, default=None,
                       help='show pie chart, integer 1 means True')
 
+    args.add_argument('-width', '--rawWidth', type=int, default=2448,
+                      help='width of raw Tif scan')
+
+    args.add_argument('-height', '--rawHeight', type=int, default=2048,
+                      help='height of raw tif scan')
+
     parsed_arg = args.parse_args()
     if parsed_arg.tifScanPath:
         predictScan(parsed_arg.tifScanPath)
     elif parsed_arg.tifVolumePath:
-        dataDict = calculateVolume(parsed_arg.tifVolumePath)
+        dataDict = calculateVolume(parsed_arg.tifVolumePath,
+                                   rawImageShape=(parsed_arg.rawWidth, parsed_arg.rawHeight))
         if parsed_arg.pie == 1:
             dict2Piechart(parsed_arg.tifVolumePath, dataDict)
     else:
